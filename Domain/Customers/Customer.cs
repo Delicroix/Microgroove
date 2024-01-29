38d45c11a991fcef131b981a4c1a9d2712e333cd
Jay <Jay@JoeWeb>
2024-01-29 10:38:34 -0600

    Add architecture

---
+++ Domain/Customers/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Customers;
+
+public class Customer
+{
+    public Guid CustomerId { get; set; }
+    public string? FullName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string? Avatar { get; set; }
+}
---
+++ Infrastructure/Context.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
