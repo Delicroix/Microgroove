20b22df984ed0a3b5b57f95dc88f3973e4de4227
Jay <Jay@JoeWeb>
2024-01-29 10:50:55 -0600

    Update NuGet packages

--- Function/Program.cs
+++ Function/Program.cs
@@ -1,7 +1,14 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
+    {
+        s.AddDbContext<Context>(options => options.UseInMemoryDatabase("InMemDb"));
+    })
     .Build();
 
 host.Run();
